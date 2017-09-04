c1ba7fedcbec935b492a0821820565af31b8e33c
butlerx <butlerx@notthe.cloud>
2017-09-04 13:26:06 +0100

    use circle v2

--- deploy/start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-isExistApp=`ps -eaf |grep cp-eventbrite-service |grep -v grep| awk '{ print $2; }'`
-if [[ -n $isExistApp ]]; then
-    service cp-eventbrite-service stop
-fi
-
-service cp-eventbrite-service start
