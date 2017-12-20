bd3ba1e6e99fb91740ba7953e611bb5d802571fb
butlerx <butlerx@notthe.cloud>
2017-12-20 10:27:15 +0000

    done is cb, cb is done

--- test/fixtures/event.js
+++ test/fixtures/event.js
@@ -1,12 +1,12 @@
 module.exports = [
   {
-    name       : 'Samplr',
-    url        : 'aak.com',
+    name: 'Samplr',
+    url: 'aak.com',
     callbackUrl: 'http://localhost:3002/auth/example-oauth2orize/callback',
-    iconUrl    : 'http://www.namiscorporate.com/images/D3.png', // ;')
-    tosUrl     : 'null.com',
-    privacyUrl : 'null.com',
-    secretKey  : 'ssh-secret',
-    fields     : ['profile.email'],
+    iconUrl: 'http://www.namiscorporate.com/images/D3.png', // ;')
+    tosUrl: 'null.com',
+    privacyUrl: 'null.com',
+    secretKey: 'ssh-secret',
+    fields: ['profile.email'],
   },
 ];
