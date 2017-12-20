ce87a8f65701e055baa2d9484ceb3fbebc3c6a74
Cian Butler <butlerx@notthe.cloud>
2017-12-20 11:00:13 +0000

    es6 upgrade (#16)
    * only run npm install on first setup
    
    * es6 upgrade
    
    * fix tests
    
    * es6 upgrade
    
    * done is cb, cb is done

--- cd-eventbrite.js
+++ cd-eventbrite.js
@@ -1,65 +1,76 @@
-var _ = require('lodash');
-var Promise = require('bluebird');
-module.exports = function () {
-  // https://github.com/senecajs/seneca/issues/112
-  var seneca = this.root;
-  var auth = require('./lib/eventbrite/entities/auth').bind(seneca)();
-  var event = require('./lib/eventbrite/entities/event').bind(seneca)();
-  var webhook = require('./lib/eventbrite/entities/webhook').bind(seneca)();
-  var acts = {};
-  var plugin = 'cd-eventbrite';
-  var senecaActP = Promise.promisify(seneca.act, {context: seneca});
+const _ = require('lodash');
+const { promisify } = require('bluebird');
+const authController = require('./lib/eventbrite/controllers/auth');
+const webhookController = require('./lib/eventbrite/controllers/webhook');
+const authEntity = require('./lib/eventbrite/entities/auth');
+const eventEntity = require('./lib/eventbrite/entities/event');
+const webhookEntity = require('./lib/eventbrite/entities/webhook');
+const ping = require('./lib/ping');
 
-  var keyCb = function (entityName, key) {
-    return function (args) {
-      return senecaActP(_.extend({
+module.exports = function cdEventbrite() {
+  // https://github.com/senecajs/seneca/issues/112
+  const seneca = this.root;
+  const auth = authEntity.bind(seneca)();
+  const event = eventEntity.bind(seneca)();
+  const webhook = webhookEntity.bind(seneca)();
+  const acts = {};
+  const plugin = 'cd-eventbrite';
+  const senecaActP = promisify(seneca.act, { context: seneca });
+
+  function keyCb(entityName, key) {
+    return args => senecaActP(
+      _.extend({
         role: plugin,
         entity: entityName,
-        cmd: key
-      }, args));
-    };
-  };
+        cmd: key,
+      }, args),
+    );
+  }
 
   // Load primitives
-  [auth, event, webhook].forEach(function (entity) {
+  [auth, event, webhook].forEach((entity) => {
     acts[entity.name] = {};
-    for (var key in entity.acts) {
-      var act = _.extend({
+    Object.entries(entity.acts).forEach(([key, { validation, cb }]) => {
+      const act = _.extend({
         role: plugin,
         entity: entity.name,
-        cmd: key
-      }, entity.acts[key].validation);
-      seneca.add(act, entity.acts[key].cb);
+        cmd: key,
+      }, validation);
+      seneca.add(act, cb);
       // Add a promise shortcut for controllers
       acts[entity.name][key] = keyCb(entity.name, key);
-      seneca.log.debug('added act role:' + entity.name + ' cmd:' + key);
-    }
+      seneca.log.debug(`added act role:${entity.name} cmd:${key}`);
+    });
   });
 
   // Load controllers
-  var ctrls = {};
-  ctrls['auth'] = require('./lib/eventbrite/controllers/auth/index').bind(seneca)();
-  ctrls['webhook'] = require('./lib/eventbrite/controllers/webhook/index').bind(seneca)();
-  _.each(ctrls, function (ctrl, entity) {
-    for (var key in ctrl.acts) {
-      var act = _.extend({
+  const ctrls = {
+    auth: authController.bind(seneca)(),
+    webhook: webhookController.bind(seneca)(),
+  };
+  _.each(ctrls, (ctrl, entity) => {
+    Object.entries(ctrl.acts).forEach(([key, { validation, cb }]) => {
+      const act = _.extend({
         role: plugin,
         ctrl: entity,
-        cmd: key
-      }, ctrl.acts[key].validation);
-      seneca.add(act, ctrl.acts[key].cb);
-      seneca.log.info('added act', act, {joi$: ctrl.acts[key].validation});
-      // No promise are added, we shouldn't have to reuse the same function twice. If we do, create an utility
-    }
+        cmd: key,
+      }, validation);
+      seneca.add(act, cb);
+      seneca.log.info('added act', act, { joi$: validation });
+      /*
+       * No promise are added, we shouldn't have to reuse the same function twice.
+       * If we do, create an utility
+       */
+    });
   });
 
   // Load utilities
-  seneca.add({ role: plugin, cmd: 'ping' }, require('./lib/ping'));
+  seneca.add({ role: plugin, cmd: 'ping' }, ping);
 
   return {
     name: plugin,
     exportmap: {
-      acts: acts
-    }
+      acts,
+    },
   };
 };
--- test/skeleton.js
+++ test/skeleton.js
@@ -1,38 +1,38 @@
-var seneca = require('../imports')({log: {level: 'info'}});
+const seneca = require('../imports')({log: {level: 'info'}});
 require('../network')(seneca);
-var _ = require('lodash');
-var flat = require('flat');
-var service = 'cd-eventbrite';
+const _ = require('lodash');
+const flat = require('flat');
+const service = 'cd-eventbrite';
 // ENTITIES
-var entities = {};
+const entities = {};
 entities['auth'] = require('../lib/eventbrite/entities/auth/index.js').bind(seneca)();
 entities['event'] = require('../lib/eventbrite/entities/event/index.js').bind(seneca)();
 entities['webhook'] = require('../lib/eventbrite/entities/webhook/index.js').bind(seneca)();
 // CTRLS
-var ctrls = {};
+const ctrls = {};
 ctrls['auth'] = require('../lib/eventbrite/controllers/auth/index.js').bind(seneca)();
 ctrls['webhook'] = require('../lib/eventbrite/controllers/webhook/index.js').bind(seneca)();
 
-var async = require('async');
-var _lab = require('lab');
-var lab = exports.lab = _lab.script();
-var describe = lab.describe;
-var it = lab.it;
-var expect = require('code').expect;
+const async = require('async');
+const _lab = require('lab');
+const lab = exports.lab = _lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = require('code').expect;
 
-describe('cp-eventbrite-service-controller', function () {
-  var testedEnt = {};
-  lab.before(function (done) {
-    seneca.ready(function () {
+describe('cp-eventbrite-service-controller', () => {
+  const testedEnt = {};
+  lab.before(done => {
+    seneca.ready(() => {
       done();
     });
   });
 
-  it('should register entity acts', function (done) {
-    var acts = seneca.list();
-    var expected_acts = [];
-    _.each(entities, function (entity) {
-      _.each(entity.acts, function (def, act) {
+  it('should register entity acts', done => {
+    const acts = seneca.list();
+    const expected_acts = [];
+    _.each(entities, entity => {
+      _.each(entity.acts, (def, act) => {
         expected_acts.push({role: service, entity: entity.name, cmd: act});
       });
     });
@@ -42,12 +42,12 @@ describe('cp-eventbrite-service-controller', function () {
     done();
   });
 
-  it('should register controllers acts', function (done) {
+  it('should register controllers acts', done => {
     // TODO : extend to other controllers
-    async.eachSeries(ctrls, function (ctl, cbS) {
-      async.eachOfSeries(ctl.acts, function (fixt, key, cbOS) {
-        var cmd = {role: service, ctrl: ctl.name, cmd: key};
-        var exists = seneca.has(cmd);
+    async.eachSeries(ctrls, (ctl, cbS) => {
+      async.eachOfSeries(ctl.acts, (fixt, key, cbOS) => {
+        const cmd = {role: service, ctrl: ctl.name, cmd: key};
+        const exists = seneca.has(cmd);
         seneca.log.info('2/ cmd:', cmd, ' exists', exists, typeof exists);
         expect(exists).to.be.true();
         cbOS();
@@ -56,9 +56,9 @@ describe('cp-eventbrite-service-controller', function () {
   });
 
   // Explicit perms to avoid forgetting adding some
-  it('should have as many perms as there is acts', function (done) {
-    var perms = flat(require('../lib/eventbrite/controllers/perm')(), { maxDepth: 3 });
-    var ctrlActs = _.filter(seneca.list(), function (o) { return _.has(o, 'ctrl'); });
+  it('should have as many perms as there is acts', done => {
+    const perms = flat(require('../lib/eventbrite/controllers/perm')(), { maxDepth: 3 });
+    const ctrlActs = _.filter(seneca.list(), o => _.has(o, 'ctrl'));
     // This extra check will only be doable once we do https://github.com/CoderDojo/cp-permissions-plugin/issues/8
     // var acts = _.filter(seneca.list(), {cmd: 'check_permissions'});
     // expect(_.keys(perms).length).to.be.equal(acts.length);
@@ -68,24 +68,24 @@ describe('cp-eventbrite-service-controller', function () {
 
   // // VALIDATION
   // No need to validate them all, we just want to verify the bootloader (/lib/index)
-  it('should validate entities acts with joi', function (done) {
-    seneca.act({role: 'cd-eventbrite', entity: 'event', cmd: 'get', id: {}}, function (err, app) {
+  it('should validate entities acts with joi', done => {
+    seneca.act({role: 'cd-eventbrite', entity: 'event', cmd: 'get', id: {}}, (err, app) => {
       expect(err.code).to.be.equal('act_invalid_msg');
       done();
     });
   });
   //
-  it('should validate entities acts while promised with joi', function (done) {
-    var app = seneca.export('cd-eventbrite/acts')['event'];
+  it('should validate entities acts while promised with joi', done => {
+    const app = seneca.export('cd-eventbrite/acts')['event'];
     app.get({id: {}})
-    .catch(function (err) {
+    .catch(err => {
       expect(err.code).to.be.equal('act_invalid_msg');
       done();
     });
   });
   //
-  it('should validate controllers acts with joi', function (done) {
-    seneca.act({role: 'cd-eventbrite', ctrl: 'auth', cmd: 'authorize', dojoId: {}}, function (err, app) {
+  it('should validate controllers acts with joi', done => {
+    seneca.act({role: 'cd-eventbrite', ctrl: 'auth', cmd: 'authorize', dojoId: {}}, (err, app) => {
       expect(err.code).to.be.equal('act_invalid_msg');
       done();
     });
